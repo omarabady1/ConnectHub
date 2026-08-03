1fed5b3c139fa8adfff5988b2379d0678c750c66
Omar Abady <omarabady44@gmail.com>
2026-08-03 21:25:59 +0300

    Add  essential packages & Create AppTextStyles

---
+++ lib/utils/app_text_styles.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTextStyles {
+  static final appBarTitle = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final cardTitle = GoogleFonts.inter(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+
+  static final cardSubtitle = GoogleFonts.inter(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  static final bodyText = GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.6,
+  );
+
+  static final caption = GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
+
+  static final smallLabel = GoogleFonts.inter(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+  );
+
+  static final chipLabel = GoogleFonts.inter(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+}
